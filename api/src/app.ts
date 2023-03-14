a79946b5d2cd699ebad9c2ce3913ca2b4efe65a8
HamidReza <delshad.swdn@gmail.com>
2023-03-14 19:54:54 +0100

    complete functions in services, controllers and routes for food

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,11 @@
 import Express from 'express'
 import cors from 'cors'
+import foodRouter from './routes/food'
 
 const app = Express()
 app.use(Express.json())
 app.use(cors())
+
+app.use("/food", foodRouter)
 
 export default app
--- api/src/services/food.ts
+++ api/src/services/food.ts
@@ -8,7 +8,25 @@ const getFoodList = async (): Promise<FoodDocument[]> => {
   return Food.find();
 };
 
+const getFoodById = async (id: string): Promise<FoodDocument | null> => {
+  return Food.findById(id);
+};
+
+const deleteFoodById = async (id: string): Promise<FoodDocument | null> => {
+  return Food.findByIdAndDelete(id);
+};
+
+const updateFoodById = async (
+  id: string,
+  update: Partial<FoodDocument>
+): Promise<FoodDocument | null> => {
+  return Food.findByIdAndUpdate(id, update, { new: true });
+};
+
 export default {
   createFood,
   getFoodList,
+  getFoodById,
+  deleteFoodById,
+  updateFoodById,
 };
