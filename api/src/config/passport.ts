3427f0bfa8c7d4201315398c21b5cc7a9e682fa1
Alina <alinbogz@gmail.com>
2023-03-14 22:21:04 +0100

    passport fixed

--- api/src/config/passport.ts
+++ api/src/config/passport.ts
@@ -1,10 +1,13 @@
 import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
 import dotenv from "dotenv";
+import GoogleTokenStrategy from "passport-google-id-token";
 
 import UserServices from "../services/users";
 
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
+const CLIENT_ID = process.env.CLIENT_ID as string;
+
 export const jwtStrategy = new JwtStrategy(
   {
     secretOrKey: JWT_SECRET,
@@ -18,5 +21,20 @@ export const jwtStrategy = new JwtStrategy(
       return "no user";
     }
     done(null, foundUser);
+  }
+);
+
+export const googleStrategy = new GoogleTokenStrategy(
+  { clientID: CLIENT_ID },
+  async function (parsedToken, googleId: {}, done) {
+    console.log(parsedToken, "parsedToken");
+    const userPayload = {
+      firstName: parsedToken.payload.given_name,
+      lastName: parsedToken.payload.family_name,
+      email: parsedToken.payload.email,
+    };
+
+    const user = await UserServices.findUserByEmail(userPayload);
+    done(null, user);
   }
 );
