946cf2c2b46aa54ffcf18cc7cc5c06edd6882671
HamidReza <delshad.swdn@gmail.com>
2023-03-14 22:57:14 +0100

    add functions for user and comment

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,11 +1,15 @@
-import Express from 'express'
-import cors from 'cors'
-import foodRouter from './routes/food'
+import Express from "express";
+import cors from "cors";
+import foodRouter from "./routes/food";
+import userRouter from "./routes/users";
+import commentRouter from "./routes/comments";
 
-const app = Express()
-app.use(Express.json())
-app.use(cors())
+const app = Express();
+app.use(Express.json());
+app.use(cors());
 
-app.use("/food", foodRouter)
+app.use("/food", foodRouter);
+app.use("/users", userRouter);
+app.use("/comments", commentRouter);
 
-export default app
+export default app;
--- api/src/routes/users.ts
+++ api/src/routes/users.ts
@@ -1,7 +1,7 @@
 // user router here
 
 import { Router } from "express";
-import passport from "passport";
+// import passport from "passport";
 
 import {
   getUserListController,
@@ -9,19 +9,21 @@ import {
   deleteUserByIdController,
   updateUserByIdController,
   logInWithPassword,
+  getUserByIdController,
 } from "../controllers/users";
 
 const userRouter = Router();
 
 // Call Express Methods for User Collection by userRouter
 userRouter.get("/", getUserListController);
+userRouter.get("/:id", getUserByIdController);
 userRouter.post("/", createUserController);
 userRouter.delete("/:id", deleteUserByIdController);
-userRouter.put(
-  "/:id",
-  passport.authenticate("jwt", { session: false }),
-  updateUserByIdController
-);
+// userRouter.put(
+//   "/:id",
+//   passport.authenticate("jwt", { session: false }),
+//   updateUserByIdController
+// );
 userRouter.post("/logIn", logInWithPassword);
 
 export default userRouter;
