183570060715c94c3a965ca8be89de31741e5e5f
HamidReza <delshad.swdn@gmail.com>
2023-03-14 20:20:32 +0100

    add hashed password, jsonwebtoken

--- api/src/services/users.ts
+++ api/src/services/users.ts
@@ -1,7 +1,11 @@
 import User, { UserDocument } from "../models/User";
 
-const createUser = async (user: UserDocument): Promise<UserDocument> => {
-  return user.save();
+const createUser = async (
+  user: UserDocument
+): Promise<UserDocument | string> => {
+  const foundUser = await User.findOne({ email: user.email });
+  if (!foundUser) return user.save();
+  else return "available";
 };
 
 //for Admin
@@ -18,9 +22,24 @@ const findUserByEmail = async (email: string): Promise<UserDocument | null> => {
   const foundUser = await User.findOne({ email: email });
   return foundUser;
 };
+
+const deleteUserById = async (userId: string): Promise<UserDocument | null> => {
+  const foundUser = User.findByIdAndDelete(userId);
+  return foundUser;
+};
+
+const updateUserById = async (
+  id: string,
+  update: Partial<UserDocument>
+): Promise<UserDocument | null> => {
+  return User.findByIdAndUpdate(id, update, { new: true });
+};
+
 export default {
   createUser,
   getUserList,
   getUserById,
   findUserByEmail,
+  deleteUserById,
+  updateUserById,
 };
