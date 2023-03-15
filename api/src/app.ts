d3e2a2f504502a2c943897df3dd3233c6cec9523
Alina <alinbogz@gmail.com>
2023-03-15 14:44:11 +0100

    passport.initialize added

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,10 +3,15 @@ import cors from "cors";
 import foodRouter from "./routes/food";
 import userRouter from "./routes/users";
 import commentRouter from "./routes/comments";
+import passport from "passport";
+import { jwtStrategy, googleStrategy } from "./config/passport";
 
 const app = Express();
 app.use(Express.json());
 app.use(cors());
+app.use(passport.initialize());
+passport.use(jwtStrategy);
+passport.use(googleStrategy);
 
 app.use("/food", foodRouter);
 app.use("/users", userRouter);
