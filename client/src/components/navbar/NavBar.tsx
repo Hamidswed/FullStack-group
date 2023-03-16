74a4d1c6929197e9f40021057794f8e51bc08e7c
HamidReza <delshad.swdn@gmail.com>
2023-03-16 16:33:31 +0100

    add favorite icon to navbar

--- client/src/components/navbar/NavBar.tsx
+++ client/src/components/navbar/NavBar.tsx
@@ -1,22 +1,55 @@
-import React from "react";
+import { styled, Button, IconButton, Badge, BadgeProps } from "@mui/material";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/logo.svg";
 import "./NavBar.css";
 
 function NavBar() {
+  const LoginBTN = styled(Button)({
+    color: "#fff",
+    backgroundColor: "black",
+    border: "1px solid #fff",
+    padding: "5 20px",
+    "&:hover": {
+      backgroundColor: "#fff",
+      color: "black",
+      border: "1px solid black",
+    },
+  });
+
+  const StyledBadge = styled(Badge)<BadgeProps>(() => ({
+    "& .MuiBadge-badge": {
+      right: 6,
+      top: 3,
+      padding: "0 4px",
+    },
+  }));
+
   return (
     <div className="navbar-container">
       <div className="navbar-left">
         <Link to="/">Home</Link>
-        <Link to="/">All Recipe</Link>
+        <Link to="/">All Recipes</Link>
+        <Link to="/about">About</Link>
       </div>
 
       <div className="navbar-center">
-        <img src={Logo} alt="logo" />
+        <Link to="/">
+          <img src={Logo} alt="logo" />
+        </Link>
       </div>
       <div className="navbar-right">
-        <Link to="/login">Log In</Link>
+        <Link to="/favorites">
+          <StyledBadge badgeContent="2" color="error">
+            <IconButton>
+              <FavoriteBorderIcon />
+            </IconButton>
+          </StyledBadge>
+        </Link>
         <Link to="/registration">Register</Link>
+        <Link to="/login">
+          <LoginBTN>Log In</LoginBTN>
+        </Link>
       </div>
     </div>
   );
