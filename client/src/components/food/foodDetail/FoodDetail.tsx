6ac36141d248fdd04c35b58f0818348ba06e5520
HamidReza <delshad.swdn@gmail.com>
2023-03-19 17:38:08 +0100

    add some style to detail food page

--- client/src/components/food/foodDetail/FoodDetail.tsx
+++ client/src/components/food/foodDetail/FoodDetail.tsx
@@ -4,9 +4,13 @@ import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import Snackbar from "@mui/material/Snackbar";
+import { Alert, Button, IconButton } from "@mui/material";
+
 import { Form, Formik } from "formik";
 
-import Button from "@mui/material/Button";
 import { FoodType } from "../../../types/foodType";
 import { TextField } from "@mui/material";
 import foodDetailSchema from "./foodDetailSchema";
@@ -14,9 +18,11 @@ import { url } from "../../../App";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../../redux/store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchCommentByFoodId } from "./../../../redux/thunk/comment";
 import CommentItem from "../../comment/commentItem/CommentItem";
+import { Link } from "react-router-dom";
+import { favoriteActions } from "../../../redux/slice/favorite";
 
 type PropType = {
   food: FoodType;
@@ -33,15 +39,57 @@ const initialValues: InitialType = {
 };
 
 const FoodDetail = ({ food }: PropType) => {
+  const [open, setOpen] = useState(false);
   const user = useSelector((state: RootState) => state.user.user);
   const comments = useSelector((state: RootState) => state.comment.comments);
   const dispatch = useDispatch<AppDispatch>();
+  const dispatchFav = useDispatch();
+  const alert = useSelector((state: RootState) => state.favorite.alert);
+  const favState = useSelector((state: RootState) => state.favorite.favorites);
+  let isFav = favState.some((item) => item.title === food.title);
 
   useEffect(() => {
     dispatch(fetchCommentByFoodId(food._id));
   }, [dispatch, comments, food._id]);
 
   console.log(comments, "comments");
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const addToFavorite = () => {
+    dispatchFav(favoriteActions.addToFavorite(food));
+    dispatchFav(favoriteActions.showAlert("Recipe added to favorite!"));
+    handleClick();
+  };
+  const removeFromFavorite = () => {
+    dispatch(favoriteActions.removeFromFavorite(food));
+    dispatch(favoriteActions.showAlert("Recipe removed from favorite!"));
+    handleClick();
+  };
+
+  // Favorite Handler
+  const favHandler = () => {
+    if (isFav) {
+      removeFromFavorite();
+      isFav = !isFav;
+    } else {
+      addToFavorite();
+      isFav = !isFav;
+    }
+  };
   // Function Call on Submit
   const token = localStorage.getItem("token");
 
@@ -71,7 +119,7 @@ const FoodDetail = ({ food }: PropType) => {
             <CardActionArea>
               <CardMedia
                 component="img"
-                height="450"
+                height="cover"
                 image={food.image}
                 alt={food.title}
               />
@@ -79,7 +127,7 @@ const FoodDetail = ({ food }: PropType) => {
           </Card>
         </div>
         <div className="food-title">
-          <h1>Crispy Baked Chicken Thighs</h1>
+          <h1>{food.title}</h1>
           <Typography
             variant="body2"
             color="text.secondary"
@@ -87,6 +135,23 @@ const FoodDetail = ({ food }: PropType) => {
           >
             {food.description}
           </Typography>
+          <Link to="/all-recipes">
+            <IconButton sx={{ mt: 2 }}>
+              <ArrowBackIcon />
+            </IconButton>
+          </Link>
+          <IconButton onClick={favHandler} sx={{ mt: 2 }}>
+            <FavoriteBorderIcon sx={{ color: isFav ? "red" : "gray" }} />
+          </IconButton>
+          <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+            <Alert
+              onClose={handleClose}
+              severity={isFav ? "success" : "error"}
+              sx={{ width: "100%" }}
+            >
+              {alert}
+            </Alert>
+          </Snackbar>
         </div>
 
         <div className="food-txtfields">
